0f04fcc2112ae5ff395db8396a42e9d8ae7b9e88
Johannes Groedem <johs@grdm.no>
2020-01-29 18:11:42 +0100

    Hallo Bob.

--- mycrate_wasm/src/lib.rs
+++ mycrate_wasm/src/lib.rs
@@ -3,5 +3,10 @@ use mycrate_core;
 
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
-    mycrate_core::add(a, b)
+    mycrate_core::add(a, b) + 1
+}
+
+#[wasm_bindgen]
+pub fn create_greeting(name: &str) -> String {
+    format!("Hei, {}!", name)
 }
--- mycrate_web/src/index.js
+++ mycrate_web/src/index.js
@@ -1,8 +1,13 @@
 import {polyfill} from './polyfill';
 
 (async function() {
-  await polyfill();
-  const {add} = await import("../../mycrate_wasm/pkg");
+    await polyfill();
+    const {add, create_greeting} = await import("../../mycrate_wasm/pkg");
 
-  console.log("Calculated with WebAssembly:", add(4, 6));
+    console.log("Calculated with WebAssembly:", add(4, 6));
+
+    const button = document.querySelector("button");
+    const input = document.querySelector("input");
+    button.onclick = () => alert(create_greeting(input.value));
+    button.disabled = false;
 })();
